767f96e6f540cb013895aa237dcc289a728db0c1
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-07-17 17:46:03 -0500

    Agregado boton de donar

--- lib/main.dart
+++ lib/main.dart
@@ -29,12 +29,24 @@ class HomeScreen extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Image.asset('Victori Icon.png'),
+          Image.asset(
+            'images/Victori Icon.png',
+            scale: 2.0,
+          ),
+          Text(
+            'Vita Nova',
+            style: TextStyle(
+              fontSize: 25.0,
+            ),
+          ),
+          SizedBox(
+            height: 75.0,
+          ),
           Center(
             child: RaisedButton(
               color: Colors.red,
               textColor: Colors.white,
-              padding: EdgeInsets.all(15.0),
+              padding: EdgeInsets.all(20.0),
               child: Text(
                 'Iniciar Sesión',
                 style: TextStyle(
@@ -51,7 +63,7 @@ class HomeScreen extends StatelessWidget {
             child: RaisedButton(
               color: Colors.red,
               textColor: Colors.white,
-              padding: EdgeInsets.all(15.0),
+              padding: EdgeInsets.all(20.0),
               child: Text(
                 'Registrarse',
                 style: TextStyle(
@@ -59,6 +71,28 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
               onPressed: () {},
+            ),
+          ),
+          SizedBox(
+            height: 50.0,
+          ),
+          Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                RaisedButton(
+                  color: Colors.red,
+                  padding: EdgeInsets.all(15.0),
+                  child: Text(
+                    'Donar',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ],
             ),
           ),
         ],
