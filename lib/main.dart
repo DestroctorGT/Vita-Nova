1f4129697c3fdfa0258172852e5b8482b344e3b0
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-07-17 16:18:39 -0500

    Agregado dos botones. Iniciar sesión y Registrarse

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,66 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() => runApp(MyApp());
 
+const kHomeScreen = 'Home Screen';
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      title: 'Vita Nova',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        brightness: Brightness.dark,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: kHomeScreen,
+      getPages: [
+        GetPage(name: kHomeScreen, page: () => HomeScreen()),
+      ],
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: RaisedButton(
+              color: Colors.red,
+              textColor: Colors.white,
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                'Iniciar Sesión',
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {},
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          SizedBox(
+            height: 25.0,
+          ),
+          Center(
+            child: RaisedButton(
+              color: Colors.red,
+              textColor: Colors.white,
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                'Registrarse',
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {},
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+          ),
+        ],
       ),
     );
   }
