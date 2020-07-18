019693561d01410660f45b2f996f53ab14bbb1bd
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-07-18 10:43:44 -0500

    Correciones

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'const.dart';
+import 'screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,85 +17,6 @@ class MyApp extends StatelessWidget {
       getPages: [
         GetPage(name: kHomeScreen, page: () => HomeScreen()),
       ],
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Image.asset(
-            'images/Victori Icon.png',
-            scale: 2.0,
-          ),
-          Text(
-            'Vita Nova',
-            style: TextStyle(
-              fontSize: 25.0,
-            ),
-          ),
-          SizedBox(
-            height: 75.0,
-          ),
-          Center(
-            child: RaisedButton(
-              color: Colors.red,
-              textColor: Colors.white,
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                'Iniciar Sesión',
-                style: TextStyle(
-                  fontSize: 20.0,
-                ),
-              ),
-              onPressed: () {},
-            ),
-          ),
-          SizedBox(
-            height: 25.0,
-          ),
-          Center(
-            child: RaisedButton(
-              color: Colors.red,
-              textColor: Colors.white,
-              padding: EdgeInsets.all(20.0),
-              child: Text(
-                'Registrarse',
-                style: TextStyle(
-                  fontSize: 20.0,
-                ),
-              ),
-              onPressed: () {},
-            ),
-          ),
-          SizedBox(
-            height: 50.0,
-          ),
-          Padding(
-            padding: EdgeInsets.all(15.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                RaisedButton(
-                  color: Colors.red,
-                  padding: EdgeInsets.all(15.0),
-                  child: Text(
-                    'Donar',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                    ),
-                  ),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
