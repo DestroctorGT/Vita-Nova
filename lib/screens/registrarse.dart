18e33e291d24f46e4ae7c08f2858be5e65a2c433
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-07-18 15:55:24 -0500

    Creado el codigo

---
+++ lib/screens/registrarse.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:vita_nova/const.dart';
+
+class Registrarse extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Image.asset(
+              'images/Victori Icon.png',
+              scale: 2.0,
+            ),
+          ),
+          Text(
+            'Vita Nova',
+            style: kVitaNovaTextStyle,
+          ),
+          SizedBox(
+            height: 80.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
