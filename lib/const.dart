26f9282be9d0e2353dcfe9434d2cded76674175e
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-07-18 14:13:35 -0500

    Creado el const del vita nova textstyle

--- lib/const.dart
+++ lib/const.dart
@@ -1,2 +1,8 @@
+import 'package:flutter/material.dart';
+
 const kHomeScreen = 'Home Screen';
 const kIniciarSesion = 'Iniciar Sesion';
+const kVitaNovaTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Berkshire Swash',
+);
