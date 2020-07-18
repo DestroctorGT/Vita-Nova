3e375fcb0f078a78bfcca4268b5bda8d772250e4
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-07-18 15:56:19 -0500

    Creado const con el nombre de la screen registrarse.

--- lib/const.dart
+++ lib/const.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 const kHomeScreen = 'Home Screen';
 const kIniciarSesion = 'Iniciar Sesion';
+const kRegistrarse = 'Registrarse';
 const kVitaNovaTextStyle = TextStyle(
   fontSize: 30.0,
   fontFamily: 'Berkshire Swash',
