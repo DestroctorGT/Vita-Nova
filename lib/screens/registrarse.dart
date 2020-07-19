eb16d831c41688c5b2f00961926f1a50c6828209
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-07-19 16:51:02 -0500

    Optimizacion en el codigo

--- lib/screens/registrarse.dart
+++ lib/screens/registrarse.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vita_nova/const.dart';
+import 'package:vita_nova/user_widgets/userinputwidget.dart';
 
 class Registrarse extends StatelessWidget {
   @override
@@ -8,7 +9,7 @@ class Registrarse extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Center(
+          Flexible(
             child: Image.asset(
               'images/Victori Icon.png',
               scale: 2.0,
@@ -18,9 +19,61 @@ class Registrarse extends StatelessWidget {
             'Vita Nova',
             style: kVitaNovaTextStyle,
           ),
-          SizedBox(
-            height: 80.0,
+          Padding(
+            padding: EdgeInsets.only(left: 45.0, right: 45.0, top: 10.0),
+            child: Form(
+              child: UserInputWidget(
+                inputTextName: 'Nombre',
+                obscureTextCheck: false,
+              ),
+            ),
           ),
+          Padding(
+            padding: EdgeInsets.only(left: 45.0, right: 45.0, top: 45.0),
+            child: Form(
+              child: UserInputWidget(
+                inputTextName: 'Apellido',
+                obscureTextCheck: false,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 45.0, right: 45.0, top: 45.0),
+            child: Form(
+              child: UserInputWidget(
+                inputTextName: 'Email',
+                inputType: TextInputType.emailAddress,
+                obscureTextCheck: false,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                left: 45.0, right: 45.0, top: 45.0, bottom: 45.0),
+            child: Form(
+              child: UserInputWidget(
+                inputTextName: 'Contraseña',
+                obscureTextCheck: true,
+              ),
+            ),
+          ),
+          RaisedButton(
+            padding: EdgeInsets.all(20.0),
+            color: Colors.indigoAccent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Text(
+              'Crear cuenta',
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 15.0,
+          )
         ],
       ),
     );
