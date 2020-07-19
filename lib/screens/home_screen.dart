da8405b99bbbf58a4b207533176ee6c12917ad9a
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-07-19 16:55:47 -0500

    Importado el dart file user_button al codigo

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vita_nova/const.dart';
+import 'package:vita_nova/user_widgets/user_button.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -70,36 +71,6 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class UserButton extends StatelessWidget {
-  UserButton(
-      {@required this.colorButton,
-      @required this.buttonText,
-      @required this.onPressed,
-      this.buttonPadding});
-  final Color colorButton;
-  final EdgeInsetsGeometry buttonPadding;
-  final buttonText;
-  final Function onPressed;
-  @override
-  Widget build(BuildContext context) {
-    return RaisedButton(
-      color: colorButton,
-      textColor: Colors.white,
-      padding: buttonPadding,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      child: Text(
-        buttonText,
-        style: TextStyle(
-          fontSize: 22.0,
-        ),
-      ),
-      onPressed: onPressed,
     );
   }
 }
