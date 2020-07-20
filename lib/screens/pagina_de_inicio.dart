67f3c9706a9a0dd46502af5821f118501c1893e5
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-07-20 12:01:40 -0500

    Creado el statefull widget

--- lib/screens/pagina_de_inicio.dart
+++ lib/screens/pagina_de_inicio.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class PaginaDeInicio extends StatefulWidget {
+  @override
+  _PaginaDeInicioState createState() => _PaginaDeInicioState();
+}
+
+class _PaginaDeInicioState extends State<PaginaDeInicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(),
+    );
+  }
+}
