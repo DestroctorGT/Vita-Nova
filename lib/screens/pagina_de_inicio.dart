c2aec29db739e227bf29e0c00dca1d87be946f0a
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-07-20 15:02:14 -0500

    Creado el bottom navigation bar para los iconos

--- lib/screens/pagina_de_inicio.dart
+++ lib/screens/pagina_de_inicio.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:feather_icons_flutter/feather_icons_flutter.dart';
 
 class PaginaDeInicio extends StatefulWidget {
   @override
@@ -10,6 +11,25 @@ class _PaginaDeInicioState extends State<PaginaDeInicio> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(FeatherIcons.home),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FeatherIcons.send),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FeatherIcons.plusSquare),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FeatherIcons.search),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FeatherIcons.settings),
+          ),
+        ],
+      ),
     );
   }
 }
