5f16ce27ed1bdf3cdbaa65c55b30c74ec9f44d52
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-07-20 11:48:13 -0500

    Creado nuevo const de la screen pagina de inicio

--- lib/const.dart
+++ lib/const.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 const kHomeScreen = 'Home Screen';
 const kIniciarSesion = 'Iniciar Sesion';
 const kRegistrarse = 'Registrarse';
+const kPaginaDeInicio = 'Pagina de inicio';
 const kColorButtonDefaultIndigoAccent = Colors.indigoAccent;
 const kVitaNovaTextStyle = TextStyle(
   fontSize: 30.0,
