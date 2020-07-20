5b3a1dbc83b79201d373707948f8d4af0f1c4c26
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-07-20 15:44:44 -0500

    Agregado la lista User Navigation Bar

--- lib/screens/pagina_de_inicio.dart
+++ lib/screens/pagina_de_inicio.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:feather_icons_flutter/feather_icons_flutter.dart';
+import 'package:vita_nova/const.dart';
 
 class PaginaDeInicio extends StatefulWidget {
   @override
@@ -14,28 +14,7 @@ class _PaginaDeInicioState extends State<PaginaDeInicio> {
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         iconSize: 27.0,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(FeatherIcons.home),
-            title: Text('Inicio'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(FeatherIcons.send),
-            title: Text('Mensaje'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(FeatherIcons.plusSquare),
-            title: Text('Subir'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(FeatherIcons.search),
-            title: Text('Buscar'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(FeatherIcons.settings),
-            title: Text('Ajustes'),
-          ),
-        ],
+        items: UserNavigationBar,
       ),
     );
   }
