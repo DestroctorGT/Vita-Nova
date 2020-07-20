6d2f96940bd2680f26d1b2c90cb59c4352ce0517
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-07-20 15:43:56 -0500

    Creado nueva lista de Bottom navigation bar

--- lib/const.dart
+++ lib/const.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:feather_icons_flutter/feather_icons_flutter.dart';
 
 const kHomeScreen = 'Home Screen';
 const kIniciarSesion = 'Iniciar Sesion';
@@ -9,3 +10,26 @@ const kVitaNovaTextStyle = TextStyle(
   fontSize: 30.0,
   fontFamily: 'Berkshire Swash',
 );
+
+const List<BottomNavigationBarItem> UserNavigationBar = [
+  BottomNavigationBarItem(
+    icon: Icon(FeatherIcons.home),
+    title: Text('Inicio'),
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(FeatherIcons.send),
+    title: Text('Mensaje'),
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(FeatherIcons.plusSquare),
+    title: Text('Subir'),
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(FeatherIcons.search),
+    title: Text('Buscar'),
+  ),
+  BottomNavigationBarItem(
+    icon: Icon(FeatherIcons.settings),
+    title: Text('Ajustes'),
+  ),
+];
