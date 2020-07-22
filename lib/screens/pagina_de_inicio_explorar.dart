71cd92be4214857768a9cc3d494669454f27fe22
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-07-22 17:07:00 -0500

    Codigo optimizado

--- lib/screens/pagina_de_inicio_explorar.dart
+++ lib/screens/pagina_de_inicio_explorar.dart
@@ -1,6 +1,8 @@
-import 'package:feather_icons_flutter/feather_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:vita_nova/const.dart';
+import 'package:vita_nova/user_widgets/user_top_icon.dart';
+import 'package:vita_nova/user_widgets/top_ui_pagina_inicio.dart';
+import 'package:vita_nova/user_widgets/publicacion_user.dart';
 
 class PaginaDeInicio extends StatefulWidget {
   @override
@@ -13,136 +15,9 @@ class _PaginaDeInicioState extends State<PaginaDeInicio> {
     return Scaffold(
       body: ListView(
         children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(FeatherIcons.user),
-                padding: EdgeInsets.all(10.0),
-                iconSize: 27.0,
-                onPressed: () {},
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FlatButton(
-                child: Text(
-                  'Amigos',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                  ),
-                ),
-                onPressed: () {},
-              ),
-              SizedBox(
-                width: 16.0,
-              ),
-              Text(
-                '|',
-                style: TextStyle(fontSize: 20.0),
-              ),
-              SizedBox(
-                width: 16.0,
-              ),
-              FlatButton(
-                child: Text(
-                  'Explorar',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                onPressed: () {},
-              ),
-            ],
-          ),
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Card(
-              color: Colors.black26,
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(15.0),
-                    child: Row(
-                      children: <Widget>[
-                        Text(
-                          'Nombre del usuario',
-                          style: TextStyle(fontSize: 20.0),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Image.asset(
-                    'images/Victori Icon.png',
-                    scale: 2.0,
-                  ),
-                  Text(
-                    'Descripcion de la imagen',
-                    style: TextStyle(fontSize: 20.0),
-                  ),
-                  Padding(
-                    padding:
-                        EdgeInsets.only(left: 15.0, right: 15.0, top: 25.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        IconButton(
-                          icon: Icon(
-                            FeatherIcons.messageSquare,
-                            size: 35.0,
-                          ),
-                          onPressed: () {},
-                        ),
-                        SizedBox(
-                          width: 5.0,
-                        ),
-                        IconButton(
-                          icon: Icon(
-                            FeatherIcons.send,
-                            size: 35.0,
-                          ),
-                          onPressed: () {},
-                        ),
-                        SizedBox(
-                          width: 5.0,
-                        ),
-                        IconButton(
-                          icon: Icon(
-                            FeatherIcons.share,
-                            size: 35.0,
-                          ),
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(15.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          '33 Comentarios',
-                          style: TextStyle(fontSize: 20.0),
-                        ),
-                        IconButton(
-                          icon: Icon(
-                            FeatherIcons.chevronsDown,
-                            size: 30.0,
-                          ),
-                          onPressed: () {},
-                        ),
-                        Text(
-                          '24 Compartidos',
-                          style: TextStyle(fontSize: 20.0),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
+          UserTopIcon(),
+          TopUIPaginaInicio(),
+          PublicacionUser(),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
