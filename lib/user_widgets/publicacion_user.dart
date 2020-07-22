d5dea71f10e3f2903551ebd62b5c83aa054cce37
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-07-22 17:05:52 -0500

    Creacion del codigo del UI

---
+++ lib/user_widgets/publicacion_user.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:feather_icons_flutter/feather_icons_flutter.dart';
+
+class PublicacionUser extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Card(
+        color: Colors.black26,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Row(
+                children: <Widget>[
+                  Text(
+                    'Nombre del usuario',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ],
+              ),
+            ),
+            Image.asset(
+              'images/Victori Icon.png',
+              scale: 2.0,
+            ),
+            Text(
+              'Descripcion de la imagen',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 15.0, right: 15.0, top: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(
+                      FeatherIcons.messageSquare,
+                      size: 35.0,
+                    ),
+                    onPressed: () {},
+                  ),
+                  SizedBox(
+                    width: 5.0,
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      FeatherIcons.send,
+                      size: 35.0,
+                    ),
+                    onPressed: () {},
+                  ),
+                  SizedBox(
+                    width: 5.0,
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      FeatherIcons.share,
+                      size: 35.0,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    '33 Comentarios',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      FeatherIcons.chevronsDown,
+                      size: 30.0,
+                    ),
+                    onPressed: () {},
+                  ),
+                  Text(
+                    '24 Compartidos',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
