258b7b8daf72a89ac4f20d72314eacedde01013b
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-07-25 17:29:25 -0500

    Actualizacion del UI

--- lib/screens/tu_perfil.dart
+++ lib/screens/tu_perfil.dart
@@ -10,7 +10,68 @@ class _TuPerfilState extends State<TuPerfil> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(25.0),
+            child: Text(
+              'Nombre de usuario',
+              style: kNormalTextFontSize,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 25.0, right: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 40.0,
+                  backgroundColor: Colors.white,
+                ),
+                Text(
+                  'Tienes 10 amigos',
+                  style: kNormalTextFontSize,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 55.0,
+          ),
+          Center(
+            child: Text(
+              'Descripción pequeña del usuario',
+              style: kNormalTextFontSize,
+            ),
+          ),
+          SizedBox(
+            height: 45.0,
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                padding: EdgeInsets.all(12.0),
+                color: kColorButtonDefaultIndigoAccent,
+                child: Text(
+                  'Tus publicaciones',
+                  style: kNormalTextFontSize,
+                ),
+                onPressed: () {},
+              ),
+              RaisedButton(
+                padding: EdgeInsets.all(12.0),
+                color: kColorButtonDefaultIndigoAccent,
+                child: Text(
+                  'Tus Gustos',
+                  style: kNormalTextFontSize,
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         iconSize: kIconSizeNavigationBar,
