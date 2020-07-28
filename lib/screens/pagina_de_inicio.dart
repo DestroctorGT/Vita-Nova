81a88a591d2855311864a38b083afe0e78b7b916
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-07-28 15:08:26 -0500

    Cambio de nombre de la screen

---
+++ lib/screens/pagina_de_inicio.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:vita_nova/const.dart';
+import 'package:vita_nova/user_widgets/user_top_icon.dart';
+import 'package:vita_nova/user_widgets/top_ui_pagina_inicio.dart';
+import 'package:vita_nova/user_widgets/publicacion_user.dart';
+
+class PaginaDeInicio extends StatefulWidget {
+  @override
+  _PaginaDeInicioState createState() => _PaginaDeInicioState();
+}
+
+class _PaginaDeInicioState extends State<PaginaDeInicio> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: ,
+        ),
+        body: ListView(
+          children: <Widget>[
+            UserTopIcon(),
+            TopUIPaginaInicio(),
+            PublicacionUser(),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: kColorButtonDefaultIndigoAccent,
+          iconSize: kIconSizeNavigationBar,
+          items: UserNavigationBar,
+        ),
+      ),
+    );
+  }
+}
