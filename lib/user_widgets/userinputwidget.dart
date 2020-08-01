f989d141ab06b4f139a196ba96d9f9492f87790c
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-08-01 11:01:22 -0500

    Agregado funcion onChanged al codigo

--- lib/user_widgets/userinputwidget.dart
+++ lib/user_widgets/userinputwidget.dart
@@ -4,10 +4,12 @@ class UserInputWidget extends StatelessWidget {
   UserInputWidget(
       {@required this.inputTextName,
       this.inputType,
-      @required this.obscureTextCheck});
+      @required this.obscureTextCheck,
+      this.onChanged});
   final inputTextName;
   final TextInputType inputType;
   final bool obscureTextCheck;
+  final Function onChanged;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
@@ -20,6 +22,7 @@ class UserInputWidget extends StatelessWidget {
         ),
         border: OutlineInputBorder(),
       ),
+      onChanged: onChanged,
     );
   }
 }
