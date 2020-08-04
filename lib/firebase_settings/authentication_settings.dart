96107c3b345c0ea332f2936defa0a8e5098440fb
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-08-04 14:45:35 -0500

    Cambio de nombre

--- lib/firebase_settings/authentication_settings.dart
+++ lib/firebase_settings/authentication_settings.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-class FirebaseSettings {
+class AuthenticationSettings {
   final auth = FirebaseAuth.instance;
   String correo;
   String contra;
