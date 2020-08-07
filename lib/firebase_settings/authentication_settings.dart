4214e2a0fea2ff2d02a20777cbee74b7b403f382
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-08-07 11:46:28 -0500

    Creado nuevos metodos sign in y sign out

--- lib/firebase_settings/authentication_settings.dart
+++ lib/firebase_settings/authentication_settings.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 
 class AuthenticationSettings {
   final _auth = FirebaseAuth.instance;
@@ -7,5 +6,13 @@ class AuthenticationSettings {
   Future<AuthResult> createNewAccount({String correo, String contra}) {
     return _auth.createUserWithEmailAndPassword(
         email: correo, password: contra);
+  }
+
+  Future<AuthResult> signIn({String correo, String contra}) {
+    return _auth.signInWithEmailAndPassword(email: correo, password: contra);
+  }
+
+  Future<void> signOut() {
+    return _auth.signOut();
   }
 }
