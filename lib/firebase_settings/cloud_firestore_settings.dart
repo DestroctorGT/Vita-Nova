fe005bb73d94ff099f96fced73ebdf6557828fb9
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-08-07 11:37:19 -0500

    Optimizacion del codigo

--- lib/firebase_settings/cloud_firestore_settings.dart
+++ lib/firebase_settings/cloud_firestore_settings.dart
@@ -1,14 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CloudFirestoreSettings {
-  final dataBase = Firestore.instance;
+  final _dataBase = Firestore.instance;
 
-  String nombre;
-  String apellido;
-
-  void createNewUserAccount(String correoUsuario, String nombreUsuario,
-      String apellidoUsuario) async {
-    await dataBase.collection('users').document(correoUsuario).setData({
+  void createNewUserAccount(
+      {String correoUsuario,
+      String nombreUsuario,
+      String apellidoUsuario}) async {
+    await _dataBase.collection('users').document(correoUsuario).setData({
       'nombre': nombreUsuario,
       'apellido': apellidoUsuario,
       'correo': correoUsuario,
