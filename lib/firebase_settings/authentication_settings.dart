34a81d65cee5e34d5c9808fc1d3d2cc984aa1033
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-08-07 11:38:31 -0500

    Optimizacion del codigo

--- lib/firebase_settings/authentication_settings.dart
+++ lib/firebase_settings/authentication_settings.dart
@@ -1,7 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
 class AuthenticationSettings {
-  final auth = FirebaseAuth.instance;
-  String correo;
-  String contra;
+  final _auth = FirebaseAuth.instance;
+
+  Future<AuthResult> createNewAccount({String correo, String contra}) {
+    return _auth.createUserWithEmailAndPassword(
+        email: correo, password: contra);
+  }
 }
