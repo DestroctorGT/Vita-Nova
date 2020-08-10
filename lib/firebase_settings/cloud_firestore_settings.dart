95eb0f887aafce033f0aee99cf08d03f098a3dc7
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-08-10 11:31:24 -0500

    Creado dos nuevos metodos, uno para recibir el nombre y el otro para el apellido del user

--- lib/firebase_settings/cloud_firestore_settings.dart
+++ lib/firebase_settings/cloud_firestore_settings.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:vita_nova/firebase_settings/authentication_settings.dart';
 
 class CloudFirestoreSettings {
   final _dataBase = Firestore.instance;
@@ -7,10 +8,35 @@ class CloudFirestoreSettings {
       {String correoUsuario,
       String nombreUsuario,
       String apellidoUsuario}) async {
-    await _dataBase.collection('users').document(correoUsuario).setData({
+    var user = await AuthenticationSettings().getCurrentUser();
+    _dataBase.collection('users').document(user.uid).setData({
       'nombre': nombreUsuario,
       'apellido': apellidoUsuario,
       'correo': correoUsuario,
     });
+  }
+
+  Future<String> getUserDataName() async {
+    var user = await AuthenticationSettings().getCurrentUser();
+    String userData;
+    userData = await _dataBase
+        .collection('users')
+        .document(user.uid)
+        .get()
+        .then((value) => value.data['nombre']);
+
+    return userData;
+  }
+
+  Future<String> getUserDataLastName() async {
+    var user = await AuthenticationSettings().getCurrentUser();
+    String userData;
+    userData = await _dataBase
+        .collection('users')
+        .document(user.uid)
+        .get()
+        .then((value) => value.data['apellido']);
+
+    return userData;
   }
 }
