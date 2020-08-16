2e3b755ac231c8a3633cc00258f1deb1ed129514
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-08-16 15:00:13 -0500

    Agregado los id de las screens al map

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vita_nova/screens/iniciar_sesion.dart';
+import 'package:vita_nova/screens/nueva_cuenta_agregar_descripcion.dart';
+import 'package:vita_nova/screens/nueva_cuenta_subir_foto_perfil.dart';
 import 'package:vita_nova/screens/pagina_de_inicio.dart';
 import 'package:vita_nova/screens/registrarse.dart';
 import 'screens/home_screen.dart';
@@ -17,13 +19,19 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         brightness: Brightness.dark,
       ),
-      initialRoute: kHomeScreen,
+      initialRoute: HomeScreen().id,
       getPages: [
-        GetPage(name: kHomeScreen, page: () => HomeScreen()),
-        GetPage(name: kIniciarSesion, page: () => IniciarSesion()),
-        GetPage(name: kRegistrarse, page: () => Registrarse()),
-        GetPage(name: kPaginaDeInicio, page: () => PaginaDeInicio()),
-        GetPage(name: kTuPerfil, page: () => TuPerfil()),
+        GetPage(name: HomeScreen().id, page: () => HomeScreen()),
+        GetPage(name: IniciarSesion().id, page: () => IniciarSesion()),
+        GetPage(name: Registrarse().id, page: () => Registrarse()),
+        GetPage(
+            name: NuevaCuentaSubirFotoPerfil().id,
+            page: () => NuevaCuentaSubirFotoPerfil()),
+        GetPage(
+            name: NuevaCuentaAgregarDescripcion().id,
+            page: () => NuevaCuentaAgregarDescripcion()),
+        GetPage(name: PaginaDeInicio().id, page: () => PaginaDeInicio()),
+        GetPage(name: TuPerfil().id, page: () => TuPerfil()),
       ],
     );
   }
