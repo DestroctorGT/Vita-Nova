eddee473f696c2915c4f2ecf5a6f22290f950719
Gino Tapia <destroctorgt@gmail.com>
2023-07-21 17:25:00 -0500

    feat: Componente nav creado junto con sus estilos

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <>
+      <Nav></Nav>
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
       </Routes>
